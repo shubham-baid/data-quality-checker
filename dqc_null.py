f99320d9eba9453d76247836091c3617c7570323
Shubham <shubhambaid1708@gmail.com>
2023-09-28 14:07:07 +0530

    InformationSchema.Table query fixed

--- dqc_null.py
+++ dqc_null.py
@@ -3,36 +3,30 @@ import sqlite3
 
 def check_null(db_name, table_name, columns):
     # Connect to the database
-    
     conn = mysql.connector.connect(host='localhost',port= 3306,user='root',password='root',database='test')
 
     # Create a cursor object to execute SQL queries
     cursor = conn.cursor()
 
-    #Check if number_columns already present or not (helps when passing multiple queries)
     
-    cursor.execute(f"select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = {table_name}and table_schema = {db_name};")
+    #Check if number_columns already present or not (helps when passing multiple queries)
+    cursor.execute(f"select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = '{table_name}'and table_schema = '{db_name}';")
     existing_columns = [column[0] for column in cursor.fetchall()]
 
-    
     if 'number_columns' not in existing_columns:    
         query = f"ALTER TABLE {table_name} ADD COLUMN number_columns INT AUTO_INCREMENT PRIMARY KEY;"
         cursor.execute(query)
 
-
-
     
-
     # Get the list of column names when all columns to be searched
     if(columns == '*'):
-        cursor.execute(f"select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = {table_name}and table_schema = {db_name};")
+        cursor.execute(f"select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = '{table_name}' and table_schema = '{db_name}';")
         columns = cursor.fetchall()
         str_columns = ', '.join([column[0] for column in columns])
 
          # Formulate the SQL query
-        query = f"SELECT number_columns, {str_columns} FROM {table_name} WHERE {' OR '.join([f'{col[0]} IS NULL' for col in tuple_columns])}"
-        
-    
+        query = f"SELECT number_columns, {str_columns} FROM {table_name} WHERE {' OR '.join([f'{col[0]} IS NULL' for col in columns])}"
+      
     else:
         str_columns = ', '.join(columns)
         print(str_columns)
@@ -48,7 +42,9 @@ def check_null(db_name, table_name, columns):
     print(len(null_rows))
     numerate = enumerate(null_rows)
     print(numerate)
-    # # If there are null values, print the column names and row positions
+    
+    
+    # If there are null values, print the column names and row positions
     if null_rows:
         null_columns = set()
         for row in null_rows:
@@ -62,7 +58,7 @@ def check_null(db_name, table_name, columns):
     else:
         print("No null values found in any column.")
 
-    # If there are null values, send an email
+    # If there are null values, print a message
     if null_rows:
         print("Null Values Found in {}".format(table_name))
     else:
@@ -75,6 +71,6 @@ def check_null(db_name, table_name, columns):
 # Example usage
 db_name = "test"
 table_name = "dummy_table"
-columns = ['name','age']
+columns = '*'
 
 check_null(db_name, table_name, columns)
